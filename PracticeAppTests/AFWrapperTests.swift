e5b541d418f8f3b020d1459809d90c40267d5a1a
Varindra <varindrahart@gmail.com>
2016-01-14 15:34:05 -0500

    still cant test

---
+++ PracticeAppTests/AFWrapperTests.swift
@@ -0,0 +1,28 @@
+//
+//  AFWrapperTests.swift
+//  PracticeApp
+//
+//  Created by Varindra Hart on 1/14/16.
+//  Copyright © 2016 Varindra Hart. All rights reserved.
+//
+
+import XCTest
+import Alamofire
+import PracticeApp
+
+class AFWrapperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testGetJSONFromAPI() {
+        
+    }
+}
